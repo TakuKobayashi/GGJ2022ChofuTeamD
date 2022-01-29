36c9483f051cea187e6e8bf8e2f732235ed3a16d
shirai708 <kumi0708@hotmail.com>
2022-01-29 22:54:17 +0900

    タイトルシーン遷移追加

---
+++ Assets/_shirai/Scripts/BootScenes.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Common
+{
+	public class BootScenes : MonoBehaviour
+	{
+
+		[SerializeField] bool BootLoad = true;
+		[SerializeField] string[] loadScenes;
+		
+
+		private IEnumerator Start()
+		{
+			if (BootLoad)
+			{
+				foreach (var scene in loadScenes)
+				{
+					yield return SceneManager.LoadSceneAsync(scene, LoadSceneMode.Single);
+				}
+			}
+		}
+	}
+}
---
+++ Assets/_shirai/Scripts/GameTransfer.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameTransfer : MonoBehaviour
+{
+    [SerializeField] string[] loadScenes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private bool bottonDown = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetMouseButton(0) || Input.GetMouseButton(1))
+        {
+            if(bottonDown==false)
+                StartCoroutine("Go");
+        }
+    }
+
+
+    	private IEnumerator Go()
+		{
+            bottonDown = true;
+
+                foreach (var scene in loadScenes)
+				{
+					yield return SceneManager.LoadSceneAsync(scene, LoadSceneMode.Additive);
+				}
+
+        SceneManager.UnloadScene("GameTitle");
+
+        }
+}
