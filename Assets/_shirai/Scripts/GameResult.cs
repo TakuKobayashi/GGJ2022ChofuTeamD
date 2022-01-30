ee17d362bc23feaea6f511e38713481f9cc74a7b
shirai708 <kumi0708@hotmail.com>
2022-01-30 14:36:29 +0900

    シーケンス移動修正

--- Assets/_shirai/Scripts/GameResult.cs
+++ Assets/_shirai/Scripts/GameResult.cs
@@ -6,6 +6,8 @@ public class GameResult : MonoBehaviour
 {
     [SerializeField] string[] loadScenes;
 
+    public float time = 0;
+    public float timer = 2;
 
     // Start is called before the first frame update
     void Start()
@@ -16,15 +18,20 @@ public class GameResult : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Return) || Input.GetMouseButton(0) || Input.GetMouseButton(1))
+        if (timer < time)
         {
-            if(bottonDown==false)
-                StartCoroutine("Go");
+            if (Input.GetKeyDown(KeyCode.Return) || Input.GetMouseButton(0) || Input.GetMouseButton(1))
+            {
+                if (bottonDown == false)
+                    StartCoroutine("Go");
+            }
         }
+        time += Time.deltaTime;
+
     }
 
 
-    	private IEnumerator Go()
+    private IEnumerator Go()
 		{
             bottonDown = true;
 
